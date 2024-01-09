de13ad33574fd75eee2af709832bca2c429543e1
Merry Damsko <m.damsko@jcourant.nl>
2024-01-10 00:44:27 +0100

    supressed unused warning in controller.

--- src/main/java/com/damsko/digit/controller/DigitController.java
+++ src/main/java/com/damsko/digit/controller/DigitController.java
@@ -2,10 +2,7 @@ package com.damsko.digit.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/digit")
